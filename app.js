b3ce010caf841ae26ac0750911a613ae29ee4188
robscott1 <rdavidscott1@gmail.com>
2022-04-01 14:50:59 -0700

    add new version to eb

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
+app.get('/new', (req, res) => {
+    res.send("Hello, from the new version!")
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
